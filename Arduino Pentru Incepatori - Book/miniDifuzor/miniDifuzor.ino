63929d4730459e8c3dfd5f8de585f1c4d339e437
turcuciprian <turcuciprian1@gmail.com>
2017-11-01 07:32:07 +0200

    playing with the buzzer

---
+++ Arduino Pentru Incepatori - Book/miniDifuzor/miniDifuzor.ino
@@ -0,0 +1,16 @@
+//using D3 (pin 3) for the i/o of the buzzer
+const int pinNr = 3;
+void setup() {
+  pinMode(pinNr,OUTPUT);
+}
+
+void loop() {
+  for(int i = 0; i<4000;i++){
+    tone(pinNr,i);
+    delay(2);// short enough delay to hear the fluctuation
+  }
+  for(int i = 4000; i>0;i--){
+    tone(pinNr,i);
+    delay(2);// short enough delay to hear the fluctuation
+  }
+}
